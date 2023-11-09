d540d8fd3d106070d6ea664365932aadde3e3dab
Rinor Ajeti <r4ajeti@gmail.com>
2023-11-09 13:36:30 +0100

    do changes based on unit test to limit valid inputs value

--- core/main.py
+++ core/main.py
@@ -1,4 +1,4 @@
-from helpers import combination
+from core.helpers import combination
 from itertools import permutations
 
 def countValidPaths(E, N):
@@ -16,6 +16,18 @@ def countValidRoots(freqOne, freqTwo):
     return sentence
 
 def countPaths(east, north):
+    if type(east) is not int:
+        raise TypeError("east must be an integer")
+    if east < 0:
+        raise ValueError("east must be a positive integer")
+    if east >= 1000:
+        raise ValueError("east must be less than 1000")
+    if type(north) is not int:
+        raise TypeError("north must be an integer")
+    if north < 0:
+        raise ValueError("north must be a positive integer")
+    if north >= 1000:
+        raise ValueError("north must be less than 1000")
     validPathValue = countValidPaths(east, north)
     validRootValue = countValidRoots(east, north)
     return validPathValue, validRootValue
