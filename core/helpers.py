0243e266c481644fedfcb7a8e9c775bd0d5a0ca0
Rinor Ajeti <r4ajeti@gmail.com>
2023-11-09 18:59:50 +0100

    add filter for routes that contains 3 consecutive chars

--- core/helpers.py
+++ core/helpers.py
@@ -5,3 +5,14 @@ def combination(n, k):
 
     combinationInt = math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
     return combinationInt
+
+def filterStringsByConsecutiveChars(inputList):
+    filteredList = []
+
+    for string in inputList:
+        for i in range(len(string) - 2):
+            if string[i] == string[i + 1] == string[i + 2]:
+                filteredList.append(string)
+                break
+
+    return filteredList
--- core/main.py
+++ core/main.py
@@ -1,10 +1,10 @@
-from core.helpers import combination
+from core.helpers import combination, filterStringsByConsecutiveChars
 from itertools import permutations
 
 def countValidPaths(E, N):
     return combination(E+N, N)
 
-def countValidRoots(freqOne, freqTwo):
+def countValidRoots(freqOne, freqTwo, exactlyThreeFlag=False):
     sentence = ""
     sentenceList = []
     availableLetters = "E"*freqOne + "N"*freqTwo
@@ -13,22 +13,24 @@ def countValidRoots(freqOne, freqTwo):
         word = ''.join(perm)
         sentenceList.append(word)
     sentenceList.sort()
+    if exactlyThreeFlag:
+        sentenceList = filterStringsByConsecutiveChars(sentenceList)
     sentence = ', '.join(sentenceList)
     return sentence
 
-def countPaths(east, north):
+def countPaths(east, north, exactlyThreeFlag=True):
     if type(east) is not int:
         raise TypeError("east must be an integer")
     if east < 0:
         raise ValueError("east must be a positive integer")
-    if east >= 1000:
+    if east > 1000:
         raise ValueError("east must be less than 1000")
     if type(north) is not int:
         raise TypeError("north must be an integer")
     if north < 0:
         raise ValueError("north must be a positive integer")
-    if north >= 1000:
+    if north > 1000:
         raise ValueError("north must be less than 1000")
     validPathValue = countValidPaths(east, north)
-    validRootValue = countValidRoots(east, north)
+    validRootValue = countValidRoots(east, north, exactlyThreeFlag=exactlyThreeFlag)
     return validPathValue, validRootValue
